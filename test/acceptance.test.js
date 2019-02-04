b1082f31a6a058092aa1c15d061d86e848e949f1
Thierry Deo <thierry.deo@gmail.com>
2019-02-04 12:05:13 +0100

    Add test for bulk insertion

--- test/acceptance.test.js
+++ test/acceptance.test.js
@@ -33,29 +33,6 @@ setTimeout(function () {
   process.exit(1)
 }, 60 * 1000).unref()
 
-test('replaces date in index', { timeout }, (t) => {
-  t.plan(3)
-  const index = 'pinotest-%{DATE}'
-
-  const instance = elastic({ index, type, consistency, host, port })
-  const log = pino(instance)
-
-  log.info('hello world')
-
-  instance.on('insert', (obj, body) => {
-    t.ok('data uploaded')
-
-    client.get({
-      index: index.replace('%{DATE}', new Date().toISOString().substring(0, 10)),
-      type,
-      id: obj._id
-    }, (err, response) => {
-      t.error(err)
-      t.deepEqual(response._source, body, 'obj matches')
-    })
-  })
-})
-
 test('store a log line', { timeout }, (t) => {
   t.plan(3)
 
@@ -125,6 +102,57 @@ test('store lines in bulk', { timeout }, (t) => {
     setTimeout(function () {
       client.get({
         index,
+        type,
+        id: obj._id
+      }, (err, response) => {
+        t.error(err)
+        t.deepEqual(response._source, body, 'obj matches')
+      })
+    }, refreshInterval)
+  })
+})
+
+test('replaces date in index', { timeout }, (t) => {
+  t.plan(3)
+  const index = 'pinotest-%{DATE}'
+
+  const instance = elastic({ index, type, consistency, host, port })
+  const log = pino(instance)
+
+  log.info('hello world')
+
+  instance.on('insert', (obj, body) => {
+    t.ok('data uploaded')
+
+    client.get({
+      index: index.replace('%{DATE}', new Date().toISOString().substring(0, 10)),
+      type,
+      id: obj._id
+    }, (err, response) => {
+      t.error(err)
+      t.deepEqual(response._source, body, 'obj matches')
+    })
+  })
+})
+
+test('replaces date in index during bulk insert', { timeout }, (t) => {
+  t.plan(15)
+
+  const index = 'pinotest-%{DATE}'
+  const instance = elastic({ index, type, consistency, host, port })
+  const log = pino(instance)
+
+  log.info('hello world')
+  log.info('hello world')
+  log.info('hello world')
+  log.info('hello world')
+  log.info('hello world')
+
+  instance.on('insert', (obj, body) => {
+    t.ok(obj, 'data uploaded')
+    setTimeout(function () {
+      client.get({
+        index: index.replace('%{DATE}', new Date().toISOString().substring(0, 10)),
         type,
         id: obj._id
       }, (err, response) => {
