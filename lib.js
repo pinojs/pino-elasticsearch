14036511034058efd50d6f647449ae5b5ca0231c
delvedor <tommydelved@gmail.com>
2020-05-22 11:58:06 +0200

    WIP: use client bulk indexer

--- lib.js
+++ lib.js
@@ -2,9 +2,7 @@
 
 /* eslint no-prototype-builtins: 0 */
 
-const pump = require('pump')
 const split = require('split2')
-const Writable = require('readable-stream').Writable
 const { Client } = require('@elastic/elasticsearch')
 const Parse = require('fast-json-parse')
 
@@ -30,7 +28,9 @@ function pinoElasticSearch (opts) {
         time: setDateTimeString(value)
       }
     } else {
-      value.time = setDateTimeString(value)
+      if (value['@timestamp'] === undefined) {
+        value.time = setDateTimeString(value)
+      }
     }
 
     function setDateTimeString (value) {
@@ -53,75 +53,36 @@ function pinoElasticSearch (opts) {
   const index = opts.index || 'pino'
   const buildIndexName = typeof index === 'function' ? index : null
   const type = opts.type || 'log'
-
-  const writable = new Writable({
-    objectMode: true,
-    highWaterMark: opts['bulk-size'] || 500,
-    writev: function (chunks, cb) {
-      const docs = new Array(chunks.length * 2)
-      for (var i = 0; i < docs.length; i++) {
-        if (i % 2 === 0) {
-          // add the header
-          docs[i] = {
-            index: {
-              // from Elasticsearch v8 and above, types will be removed
-              // while in Elasticsearch v7 types are deprecated
-              _type: esVersion >= 7 ? undefined : type,
-              _index: getIndexName(chunks[Math.floor(i / 2)].chunk.time)
-            }
-          }
-        } else {
-          // add the chunk
-          docs[i] = chunks[Math.floor(i / 2)].chunk
+  const b = client.helpers.bulk({
+    datasource: splitter,
+    flushBytes: opts['flush-bytes'],
+    onDocument (doc) {
+      return {
+        index: {
+          _index: getIndexName(doc.time || doc['@timestamp']),
+          _type: esVersion >= 7 ? undefined : type
         }
       }
-      client.bulk({
-        body: docs
-      }, function (err, result) {
-        if (!err) {
-          const items = result.body.items
-          for (var i = 0; i < items.length; i++) {
-            // depending on the Elasticsearch version, the bulk response might
-            // contain fields 'create' or 'index' (> ES 5.x)
-            const create = items[i].index || items[i].create
-            splitter.emit('insert', create, docs[i * 2 + 1])
-          }
-        } else {
-          splitter.emit('insertError', err)
-        }
-        // skip error and continue
-        cb()
-      })
     },
-    write: function (body, enc, cb) {
-      var idx = getIndexName(body.time)
-      // from Elasticsearch v8 and above, types will be removed
-      // while in Elasticsearch v7 types are deprecated
-      const obj = {
-        index: idx,
-        type: esVersion >= 7 ? undefined : type,
-        body: body
-      }
-      client.index(obj, function (err, data) {
-        if (!err) {
-          splitter.emit('insert', data.body, obj.body)
-        } else {
-          splitter.emit('insertError', err)
-        }
-        // skip error and continue
-        cb()
-      })
+    onDrop (doc) {
+      const error = new Error('Dropped document')
+      error.onDocument = doc
+      splitter.emit('insertError', error)
     }
   })
 
-  function getIndexName (time) {
+  b.then(
+    (stats) => splitter.emit('insert', stats),
+    (err) => splitter.emit('error', err)
+  )
+
+  function getIndexName (time = Date.now()) {
     if (buildIndexName) {
       return buildIndexName(time)
     }
     return index.replace('%{DATE}', time.substring(0, 10))
   }
 
-  pump(splitter, writable)
   return splitter
 }
 
