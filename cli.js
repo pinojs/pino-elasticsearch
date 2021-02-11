5a82f658e5d7e65828388c3c1ff8fa02a60c3791
alpacaHL39 <henrylauyu039@gmail.com>
2021-02-12 00:58:07 +0800

    update: follow standard style

--- cli.js
+++ cli.js
@@ -66,14 +66,14 @@ if (flags['read-config']) {
   if (flags['read-config'].match(/.*\.json$/) !== null) {
     const config = JSON.parse(fs.readFileSync(path.join(__dirname, flags['read-config']), 'utf-8'))
     allowedProps.forEach(key => {
-      config[key] ? flags[key] = config[key] : null
+	  if(config[key]) flags[key] = config[key]
     })
   }
 
   if (flags['read-config'].match(/.*\.js$/) !== null) {
     const config = require(path.join(__dirname, flags['read-config']))
     allowedProps.forEach(key => {
-      config[key] ? flags[key] = config[key] : null
+	  if(config[key]) flags[key] = config[key]
     })
   }
 }
