61d9f51085243d2ce8474528f5de464acfc59863
Sheldhur Mornor <drfreez@ya.ru>
2023-07-21 17:18:31 +0800

    fix reconnection (#168)
    * fix reconnection
    
    * ConnectionPool typo
    
    ---------
    
    Co-authored-by: sheldhur <sheldhur@gmail.com>

--- example.js
+++ example.js
@@ -1,31 +1,24 @@
 'use strict'
 
-const pino = require('pino')()
-
-pino.info('hello world')
-pino.error('this is at error level')
-pino.info('the answer is %d', 42)
-pino.info({ obj: 42 }, 'hello world')
-pino.info({ obj: 42, b: 2 }, 'hello world')
-pino.info({ obj2: { aa: 'bbb' } }, 'another')
-setImmediate(function () {
-  pino.info('after setImmediate')
+const pinoJs = require('pino')
+const elastic = require('./lib')({
+  index: 'pinotest',
+  node: 'http://localhost:9200',
+  auth: {
+    apiKey: 'someKey'
+  },
+  consistency: 'one',
+  'es-version': 7,
+  'flush-bytes': 1000
 })
-pino.error(new Error('an error'))
 
-const child = pino.child({ a: 'property' })
-child.info('hello child!')
+const level = 'trace'
+const pino = pinoJs({ level }, pinoJs.multistream([
+  { level, stream: elastic },
+  { level, stream: process.stdout }
+]))
 
-const childsChild = child.child({ another: 'property' })
-childsChild.info('hello baby..')
-
-pino.debug('this should be mute')
-
-pino.level = 'trace'
-
-pino.debug('this is a debug statement')
-
-pino.child({ another: 'property' }).debug('this is a debug statement via child')
-pino.trace('this is a trace statement')
-
-pino.debug('this is a "debug" statement with "')
+pino.trace('test')
+setInterval(function () {
+  pino.trace('test')
+}, 10000)
