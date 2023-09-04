719be97fbdcf010b6823a4b9184e5157807a2606
NSRB <nasyarobby@gmail.com>
2023-09-04 22:32:19 +0700

    refactored for unit testing

--- cli.js
+++ cli.js
@@ -45,13 +45,48 @@ function start (opts) {
     console.error('Elasticsearch server error:', error)
   })
 
-  if (opts.rejectUnauthorized) {
-    opts.rejectUnauthorized = opts.rejectUnauthorized !== 'false'
-  }
   pump(process.stdin, stream)
 }
 
-const flags = minimist(process.argv.slice(2), {
+function startCli (flags) {
+  const allowedProps = [
+    'node',
+    'index',
+    'flush-bytes',
+    'flush-interval',
+    'trace-level',
+    'username',
+    'password',
+    'api-key',
+    'cloud',
+    'es-version',
+    'rejectUnauthorized'
+  ]
+
+  if (flags['read-config']) {
+    if (flags['read-config'].match(/.*\.json$/) !== null) {
+      const config = JSON.parse(fs.readFileSync(path.join(process.cwd(), flags['read-config']), 'utf-8'))
+      allowedProps.forEach(key => {
+        if (config[key] !== undefined) {
+          flags[key] = config[key]
+        }
+      })
+    }
+    if (flags['read-config'].match(/.*\.js$/) !== null) {
+      const config = require(path.join(process.cwd(), flags['read-config']))
+      allowedProps.forEach(key => {
+        if (config[key] !== undefined) {
+          flags[key] = config[key]
+        }
+      })
+    }
+  }
+
+  start(flags)
+}
+
+if (require.main === module) {
+  startCli(minimist(process.argv.slice(2), {
   alias: {
     version: 'v',
     help: 'h',
@@ -69,23 +104,7 @@ const flags = minimist(process.argv.slice(2), {
   default: {
     node: 'http://localhost:9200'
   }
-})
-
-const allowedProps = ['node', 'index', 'flush-bytes', 'flush-interval', 'trace-level', 'username', 'password', 'api-key', 'cloud', 'es-version', 'rejectUnauthorized']
-
-if (flags['read-config']) {
-  if (flags['read-config'].match(/.*\.json$/) !== null) {
-    const config = JSON.parse(fs.readFileSync(path.join(process.cwd(), flags['read-config']), 'utf-8'))
-    allowedProps.forEach(key => {
-      if (config[key]) { flags[key] = config[key] }
-    })
+  }))
   }
 
-  if (flags['read-config'].match(/.*\.js$/) !== null) {
-    const config = require(path.join(process.cwd(), flags['read-config']))
-    allowedProps.forEach(key => {
-      if (config[key]) { flags[key] = config[key] }
-    })
-  }
-}
-start(flags)
+module.exports = startCli
